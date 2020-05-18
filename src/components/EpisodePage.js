1e21ceba8977908b195da9035e89aa8f3a67080a
unknown <vincent.truong95@gmail.com>
2020-05-18 23:58:01 +0100

    added detail to footer, need to do mobile version

--- src/components/EpisodePage.js
+++ src/components/EpisodePage.js
@@ -7,42 +7,30 @@ class EpisodePage extends Component {
 
     render() {
         return(
-            <div>
-				<section id="header" className="headerNoPadding">					
-					<h1><Link to="/" className="MIC">Made in Chafford</Link></h1>
-					<nav id="nav">
-						<ul>
-							<li><Link to="/" className="Home">Home</Link></li>
-							<li>< Link to="/episodes" className="Episodes">Episodes</Link></li>
-						</ul>
-					</nav>
-				</section>
+			<section id="main">
+				<div className="container">
+					<article className="box post">
+						<a href="#" className="image featured"><img src="/images/pic01.jpg" alt="" /></a>
+						<header>
+							<h2>Currently listening to</h2>
+							<p></p>
+						</header>
+						<p>
+							<SoundCloudBlock index={this.props.location.pathname.split('episode/').pop()} />
+						</p>
+						<p className="podcastLinks">
+							<a className ="spotifyPodcasts" target="_blank" href="https://open.spotify.com/show/3WH7F5Br5i9AQGSugnlDHF">
+								<img className ="spotifyPodcastsImage"src="/images/spotifylisten.webp" alt="" />
+							</a>
+							<a className ="applePodcasts" target="_blank" href="https://podcasts.apple.com/gb/podcast/made-in-chafford/id1235162230">
+								<img className ="applePodcastsImage" src="/images/applelisten.svg" alt="" />
+							</a>
+						</p>
+						<PodcastFeed variant="single" episodeId={this.props.location.pathname.split('episode/').pop()}/>
+					</article>
 
-				<section id="main">
-					<div className="container">
-						<article className="box post">
-							<a href="#" className="image featured"><img src="/images/pic01.jpg" alt="" /></a>
-							<header>
-								<h2>Currently listening to</h2>
-								<p></p>
-							</header>
-							<p>
-								<SoundCloudBlock index={this.props.location.pathname.split('episode/').pop()} />
-							</p>
-							<p className="podcastLinks">
-								<a className ="spotifyPodcasts" target="_blank" href="https://open.spotify.com/show/3WH7F5Br5i9AQGSugnlDHF">
-									<img className ="spotifyPodcastsImage"src="/images/spotifylisten.webp" alt="" />
-								</a>
-								<a className ="applePodcasts" target="_blank" href="https://podcasts.apple.com/gb/podcast/made-in-chafford/id1235162230">
-									<img className ="applePodcastsImage" src="/images/applelisten.svg" alt="" />
-								</a>
-							</p>
-							<PodcastFeed variant="single" episodeId={this.props.location.pathname.split('episode/').pop()}/>
-						</article>
-
-					</div>
-				</section>
-            </div>
+				</div>
+			</section>
         )
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,19 @@ class Header extends Component {
 
     render() {
         return(
-            <section id="header">				
-                <h1><Link to="/" className="MIC">Made in Chafford</Link></h1>
-                <nav id="nav">
-                    <ul>
-                        <li><Link to="/" className="Home">Home</Link></li>
-                        <li>< Link to="/episodes" className="Episodes">Episodes</Link></li>
-                    </ul>
-                </nav>
-			</section>
+            <div>
+                <section id="header">				
+                    <h1><Link to="/" className="MIC">Made in Chafford</Link></h1>
+                    <nav id="nav">
+                        <ul>
+                            <li><Link to="/" className="Home">Home</Link></li>
+                            <li>< Link to="/episodes" className="Episodes">Episodes</Link></li>
+                        </ul>
+                    </nav>
+                </section>
+                
+                <section id="header" className="headerNoPadding" />					
+            </div>
         )
     }
 }
