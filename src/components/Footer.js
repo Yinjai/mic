13dfeffd80c421dcac374c197150eeaf53a210e7
unknown <vincent.truong95@gmail.com>
2020-05-19 18:42:34 +0100

    footer mobile compatible added links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,20 +10,14 @@ class Footer extends Component {
                         <div className="footerSection">
                             <section>
                                 <ul className="social">
-                                    <li><a className="icon brands fa-facebook-f" href="#"><span className="label">Facebook</span></a></li>
-                                    <li><a className="icon brands fa-twitter" href="#"><span className="label">Twitter</span></a></li>
-                                    <li><a className="icon brands fa-instagram" href="#"><span className="label">Instagram</span></a></li>
+                                    <li><a className="icon brands fa-facebook-f" target="_blank" href="https://www.facebook.com/MadeInChafford/"><span className="label">Facebook</span></a></li>
+                                    <li><a className="icon brands fa-twitter" target="_blank" href="https://twitter.com/madeinchafford"><span className="label">Twitter</span></a></li>
+                                    <li><a className="icon brands fa-instagram" target="_blank" href="https://www.instagram.com/madeinchafford/"><span className="label">Instagram</span></a></li>
                                 </ul>
                                 <ul className="contact">
                                     <li>
                                         <h3>Enquiries</h3>
-                                        <p>
-                                            madeinchafford@gmail.com
-                                        </p>
-                                    </li>
-                                    <li>
-                                        <h3>Links</h3>
-                                        <p><a href="#">someone@untitled.tld</a></p>
+                                        <p><a href="mailto:madeinchafford@gmail.com">madeinchafford@gmail.com</a></p>
                                     </li>
                                     <li>
                                         <h3>Phone</h3>
