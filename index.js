983a5dc55270b4a00aef63a8a07ca08cc60d94f9
unknown <vincent.truong95@gmail.com>
2020-05-31 15:02:03 +0100

    trying to resolve 404 error in post request

--- index.js
+++ index.js
@@ -10,43 +10,44 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 router.post('/send', (req, res) => {
-    const htmlEmail = `
-        <h3>Contact Details</h3>
-        <ul>
-            <li>Name: ${req.body.name}</li>
-            <li>Email: ${req.body.email}</li>
-        </ul>
-        <h3>Message</h3>
-        <p>Name: ${req.body.message}</p>
-    `
+    nodemailer.createTestAccount((err, account) => {
+        const htmlEmail = `
+            <h3>Contact Details</h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>Name: ${req.body.message}</p>
+        `
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email', 
-        port: 587,
-        auth: {
-            user: "elenora.kulas@ethereal.email",
-            pass: "Ewn9u12xVwEBXfVsBK"
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email', 
+            port: 587,
+            auth: {
+                user: "elenora.kulas@ethereal.email",
+                pass: "Ewn9u12xVwEBXfVsBK"
+            }
+        });
+
+        let mailOptions = {
+            from: 'test@testaccount.com',
+            to: 'elenora.kulas@ethereal.email',  
+            subject: 'New Message from Contact Form',
+            replyTo: 'test@testaccount.com',
+            subject: "New message",
+            text: req.body.message,
+            html: htmlEmail
         }
-    });
 
-    let mailOptions = {
-        from: 'test@testaccount.com',
-        to: 'elenora.kulas@ethereal.email',  
-        subject: 'New Message from Contact Form',
-        replyTo: 'test@testaccount.com',
-        subject: "New message",
-        text: req.body.message,
-        html: htmlEmail
-    }
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(err) {
-            return console.log(err)
-        }
-        console.log("Message sent: %s", info)
-        console.log("Message URL: %s", nodemailer.getTestMessageUrl(info))
-    });
-
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err) {
+                return console.log(err)
+            }
+            console.log("Message sent: %s", info)
+            console.log("Message URL: %s", nodemailer.getTestMessageUrl(info))
+        });
+    })
     res.send("message sent here");
 })
 
