53dce37cafa53c8e23eaa7f831618f18fb7ce959
unknown <vincent.truong95@gmail.com>
2020-06-17 00:38:15 +0100

    gmail message

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-// const { google } = require("googleapis");
-// const OAuth2 = google.auth.OAuth2;
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 const app = express();
 const router = express.Router();
 
@@ -23,41 +23,41 @@ router.post('/send', (req, res) => {
         <h3>Message</h3>
         <p>Name: ${req.body.message}</p>
     `
-    // const oauth2Client = new OAuth2(
-    //     process.env.CLIENTID,
-    //     process.env.CLIENTSECRET,
-    //     "https://developers.google.com/oauthplayground"
-    // );
+    const oauth2Client = new OAuth2(
+        process.env.CLIENTID,
+        process.env.CLIENTSECRET,
+        "https://developers.google.com/oauthplayground"
+    );
 
-    // oauth2Client.setCredentials({
-    //     refresh_token: process.env.REFRESHTOKEN
-    // });
-    // const accessToken = oauth2Client.getAccessToken()
-
-
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email', 
-        port: 587,
-        auth: {
-            user: process.env.USER,
-            pass: process.env.PASS
-        }
+    oauth2Client.setCredentials({
+        refresh_token: process.env.REFRESHTOKEN
     });
+    const accessToken = oauth2Client.getAccessToken()
+
 
     // let transporter = nodemailer.createTransport({
-    //     service: "gmail",
+    //     host: 'smtp.ethereal.email', 
+    //     port: 587,
     //     auth: {
-    //         type: "OAuth2",
-    //         user: "madeinchafford@gmail.com", 
-    //         clientId: process.env.CLIENTID,
-    //         clientSecret: process.env.CLIENTSECRET,
-    //         refreshToken: process.env.REFRESHTOKEN,
-    //         accessToken: accessToken
-    //     },
-    //     tls: {
-    //         rejectUnauthorized: false
+    //         user: process.env.USER,
+    //         pass: process.env.PASS
     //     }
     // });
+
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: "madeinchafford@gmail.com", 
+            clientId: process.env.CLIENTID,
+            clientSecret: process.env.CLIENTSECRET,
+            refreshToken: process.env.REFRESHTOKEN,
+            accessToken: accessToken
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
     let mailOptions = {
         from: req.body.email,
