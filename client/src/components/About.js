2a3bf90226a25e1a59cb53b18c8bea58efaf4f75
unknown <vincent.truong95@gmail.com>
2020-06-21 20:51:39 +0100

    Updated about us with content

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,24 +3,39 @@ import Loader from '../functional/Loader';
 
 class About extends Component {
 
-    render() {        
-        return(
-			<section id="main">
-				<div className="container">
+    render() {
+        return (
+            <section id="main">
+                <div className="container">
                     <article className="box post">
-                        {/* <img className="episodeThumbnail" src={this.props.thumbnail.replace('http', 'https')} alt="" /> */}
                         <header>
                             <h3>About us</h3>
                         </header>
-                            <p>Placeholder</p>
-                            <p>Placeholder</p>
-                            <p>
-                                Made in Chafford <a href="https://twitter.com/madeinchafford">Twitter</a> & <a href="https://www.instagram.com/madeinchafford/">Instagram</a>
-                            </p>
+                        <p>
+                            Made In Chafford is a podcast run by 3 guys
+                            from Essex who talk Music, Pop Culture & Entertainment.
+                            The idea was born from a drunken idea on a uni night out and
+                            blossomed into a fully fledged podcast,
+                            which has been running for 4 years strong.
+                            With over 30K listens on all platforms and a host of amazing guests,
+                            we have strived every week to give you quality content.
+                            MIC itself is a group of friends which
+                            formed back in secondary school.
+                        </p>
+                        <p>
+                            We created this space to also have people from all
+                            walks of life to join us in conversation.
+                            We believe it’s important that everyone is given a voice, so if you want to appear
+                            on the podcast or have any other requests please reach out to the hosts
+                            individually or through the contact form below.
+                        </p>
+                        <p>
+                            Catch us on: <a href="https://twitter.com/madeinchafford">Twitter</a> & <a href="https://www.instagram.com/madeinchafford/">Instagram</a>
+                        </p>
                     </article>
                     <Loader />
-				</div>
-			</section>
+                </div>
+            </section>
         )
     }
 }
