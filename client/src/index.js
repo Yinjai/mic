0a5d758bc3fd927a172d761fcddd076019e7ac59
unknown <vincent.truong95@gmail.com>
2020-06-24 01:07:55 +0100

    added 404 page with switch

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Home from './components/Home.js';
 import EpisodePage from './components/EpisodePage.js';
 import Episodes from './components/Episodes.js';
 import Header from './components/Header.js';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ScrollToTop from './functional/ScrollToTop.js';
 import MICPlaylist from './components/MICPlaylist.js';
 import About from './components/About.js';
@@ -18,12 +18,14 @@ const App = () => {
             <BrowserRouter>
                 <ScrollToTop />
                 <Header />
-                <Route path="/" exact component={Home} />
-                <Route path="/episodes/page/:page" exact component={() => <Episodes variant="list" />} />
-                <Route path="/episode/:episodeId" component={EpisodePage} />
-                <Route path="/micplaylist" component={MICPlaylist} />
-                <Route path="/about" component={About} />
-                <Route path="*" component={ErrorPage} />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/episodes/page/:page" exact component={() => <Episodes variant="list" />} />
+                    <Route path="/episode/:episodeId" component={EpisodePage} />
+                    <Route path="/micplaylist" component={MICPlaylist} />
+                    <Route path="/about" component={About} />
+                    <Route path="*" component={ErrorPage} />
+                </Switch>
             </BrowserRouter>
             <Footer />
         </div>
